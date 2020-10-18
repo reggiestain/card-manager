3ce8f7ff22ff5ce42c5613fc3e411e36e03b2c49
devstain <reginald.agilemotion.co.za>
2020-10-19 01:59:40 +0200

    Removed vendor folder

--- card/vendor/alimranahmed/laraocr/src/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::get('ocr', 'Alimranahmed\LaraOCR\Controllers\OcrController@home')->name('home');
-
-Route::post('ocr', 'Alimranahmed\LaraOCR\Controllers\OcrController@readImage');
-
-Route::get('ocr/test', 'Alimranahmed\LaraOCR\Controllers\OcrController@test');

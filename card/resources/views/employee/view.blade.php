6263a01f28aed8af2c79546e4d90042c65dad33c
devstain <reginald.agilemotion.co.za>
2020-10-20 13:14:36 +0200

    fixed pdf image base path

--- card/resources/views/employee/view.blade.php
+++ card/resources/views/employee/view.blade.php
@@ -71,7 +71,7 @@
                                                     </td>
                                                     <td colspan="3">
                                                         @if ($employee->profile_image)
-                                                        <img src="{{asset($employee->profile_image)}}" alt="profile" class="img-thumbnail" width="200" height="200" />
+                                                        <img src="{{asset('card/storage/app/public'.$employee->profile_image)}}" alt="profile" class="img-thumbnail" width="200" height="200" />
                                                         @else
 
                                                         @endif

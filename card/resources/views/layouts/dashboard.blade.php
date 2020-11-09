14e7d022fa1f60451cb4d107e76499a84cd90f44
devstain <reginald.agilemotion.co.za>
2020-11-09 14:04:03 +0200

    file upload feature

--- card/resources/views/layouts/dashboard.blade.php
+++ card/resources/views/layouts/dashboard.blade.php
@@ -261,9 +261,9 @@
     <!-- Bootstrap core JavaScript-->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/EZView.js') }}"></script>
     <script src="{{ asset('js/scripts.js') }}"></script>
     <script src="{{ asset('js/jquery.fileuploader.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('js/EZView.js') }}"></script>
     <script src="{{ asset('js/draggable.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
@@ -281,9 +281,6 @@
             $('input[name="files"]').fileuploader({
                 addMore: true
             });
-
-            $('.ezv').EZView();
-
 
             $(document).on('click', '.pdf-view', function(e) {
                 e.preventDefault();
@@ -602,6 +599,12 @@
             }
 
         });
+
+        $(function(){
+         $('ezv').EZView();
+
+        });
+
 
 </script>
 </body>

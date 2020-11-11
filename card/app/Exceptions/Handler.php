5284d2f0bdc4962fa2dcfd4dc2d62b001067ec69
devstain <reginald.agilemotion.co.za>
2020-11-11 19:14:24 +0200

    Register form update

--- card/app/Exceptions/Handler.php
+++ card/app/Exceptions/Handler.php
@@ -46,6 +46,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof AuthenticationException) {
+            return redirect('/');
+        }
         return parent::render($request, $exception);
     }
 }

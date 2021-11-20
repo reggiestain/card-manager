dfb87343fc765b2be768204affdf81a1767c330e
devstain <reginald.agilemotion.co.za>
2021-11-21 00:15:18 +0200

    out of service

--- card/routes/web.php
+++ card/routes/web.php
@@ -14,14 +14,14 @@
 Route::get('/', function () {
     return view('auth.login');
 });
-
+exit();
 Auth::routes();
 //report
 //Route::get('/','Reports@agePerFarmer');
-Route::get('/avgCrop','Reports@seedlingsPerCrop');
-Route::get('/avgDependant','Reports@avgDependant');
-Route::get('/seedlingsPerCrop','Reports@seedlingsPerCrop')->name('report.cropsum');
-Route::get('/seedlingsPerDistrict','Reports@seedlingsPerDistrict')->name('report.distsum');
+Route::get('/avgCrop', 'Reports@seedlingsPerCrop');
+Route::get('/avgDependant', 'Reports@avgDependant');
+Route::get('/seedlingsPerCrop', 'Reports@seedlingsPerCrop')->name('report.cropsum');
+Route::get('/seedlingsPerDistrict', 'Reports@seedlingsPerDistrict')->name('report.distsum');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/institution/add', 'InstitutionController@create')->name('institution.add');
 Route::get('/institution/index', 'InstitutionController@index')->name('institution.index');
@@ -36,12 +36,11 @@ Route::get('/employee/delete/{id}', 'EmployeeController@destroy')->name('employe
 Route::get('/employee/pdf/{id}', 'EmployeeController@pdf')->name('employee.pdf');
 Route::post('/employee/update/{id}', 'EmployeeController@update')->name('employee.update');
 Route::post('/employee/store', 'EmployeeController@store')->name('employee.store');
-Route::resource('Institution', 'InstitutionController',['except'=>['show','create','store']]);
-Route::resource('employee', 'EmployeeController',['except'=>['show','create','store','index']]);
+Route::resource('Institution', 'InstitutionController', ['except'=>['show','create','store']]);
+Route::resource('employee', 'EmployeeController', ['except'=>['show','create','store','index']]);
 
-Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
-   Route::resource('users', 'UserController',['except'=>['show','create','store']]); 
-   
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+    Route::resource('users', 'UserController', ['except'=>['show','create','store']]);
 });
 
 //OCR
